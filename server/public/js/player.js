52aaceb914749adff40ec858d32d4fc9ba8904c1
J3anSimas <jeansimas16@gmail.com>
2024-11-05 00:11:18 -0300

    Adicionando JSDocs e player.js

---
+++ server/public/js/player.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef {Object} Player
+ * @property {string} Id
+ * @property {boolean} Ready
+ * @property {boolean} IsHost
+ * @property {Point} Position
+ * @property {number} Health
+ * @property {number} Coins
+ * @property {number} MoveCapacity
+ * @property {number} MovesRemaining
+ * @property {number} Strength
+ * @property {number} TotalShots
+ * @property {number} ShotsRemaining
+ * @property {boolean} IsDead
+ */
+
